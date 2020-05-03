a3fbf6327c3e98a9fc5e0cd91d2c5be263974075
multilinguist <xuxiaodong2007@gmail.com>
2020-05-03 09:04:40 +0800

    DCE fix bug

--- getFuturePositionRank.py
+++ getFuturePositionRank.py
@@ -69,6 +69,7 @@ def parseDCE(date, datafile):
 	filelist = os.listdir("./dce")
 	csv_write = csv.writer(open("vorank_dce.csv",'w+',encoding='GBK',newline=''))
 	csv_write.writerow(header)
+	allrows = []
 	products = []
 	exchange = "dce"
 	for i in range(0,len(filelist)):
@@ -117,7 +118,8 @@ def parseDCE(date, datafile):
 						increment = bigint(items[3])
 					row = [date,product,contract,exchange,institution,rank,volume,increment,datatype]
 					if row:
-						csv_write.writerow(row)
+						#csv_write.writerow(row)
+						allrows.append(row)
 	#products = ["J"]
 	for product in products:
 		content = getDcePostData(date,product)
@@ -132,23 +134,23 @@ def parseDCE(date, datafile):
 		if match1:
 			table = match1.groupdict()["table"]
 			#print(table)
-			strPattern2 = "<tr >      <td >[0-9]+</td><td >.{2,20}</td><td  class=\"td-right\">[0-9,]+</td><td  class=\"td-right\">[0-9,]+</td>             \
-<td >[0-9]+</td><td >.{2,20}</td><td  class=\"td-right\">[0-9,]+</td><td  class=\"td-right\">[0-9,]+</td>              \
-<td >[0-9]+</td><td >.{2,20}</td><td  class=\"td-right\">[0-9,]+</td><td  class=\"td-right\">[0-9,]+</td>              </tr>"
+			strPattern2=r"<tr >\s{1,10}<td\s?>.{1,10}</td><td\s?>.{2,20}</td><td\s{1,10}class=\"td-right\">[0-9,]+</td><td\s{1,10}class=\"td-right\">[0-9,]+</td>\s+"
+			strPattern2+=r"<td\s?>.{1,10}</td><td\s?>.{2,20}</td><td\s{1,10}class=\"td-right\">[0-9,]+</td><td\s{1,10}class=\"td-right\">[\d,-]+</td>\s+"
+			strPattern2+=r"<td\s?>.{1,10}</td><td\s?>.{2,20}</td><td\s{1,10}class=\"td-right\">[0-9,]+</td><td\s{1,10}class=\"td-right\">[\d,-]+</td>\s{1,16}</tr>"
 			match2 = re.findall(strPattern2,table)
 			print(len(match2))
 			for item in match2:
-				strPattern3 = "<tr >      <td >(?P<No1>[0-9]+)</td><td >(?P<Name1>.{2,20})</td><td  class=\"td-right\">(?P<Volume1>[0-9,]+)</td><td  class=\"td-right\">(?P<Diff1>[0-9,]+)</td>             \
-<td >(?P<No2>[0-9]+)</td><td >(?P<Name2>.{2,20})</td><td  class=\"td-right\">(?P<Volume2>[0-9,]+)</td><td  class=\"td-right\">(?P<Diff2>[0-9,]+)</td>              \
-<td >(?P<No3>[0-9]+)</td><td >(?P<Name3>.{2,20})</td><td  class=\"td-right\">(?P<Volume3>[0-9,]+)</td><td  class=\"td-right\">(?P<Diff3>[0-9,]+)</td>              </tr>"
+				strPattern3=r"<tr >\s{1,10}<td\s?>(?P<No1>.{1,10})</td><td\s?>(?P<Name1>.{2,20})</td><td\s{1,10}class=\"td-right\">(?P<Volume1>[0-9,]+)</td><td\s{1,10}class=\"td-right\">(?P<Diff1>[0-9,]+)</td>\s+"
+				strPattern3+=r"<td\s?>(?P<No2>.{1,10})</td><td\s?>(?P<Name2>.{2,20})</td><td\s{1,10}class=\"td-right\">(?P<Volume2>[0-9,]+)</td><td\s{1,10}class=\"td-right\">(?P<Diff2>[\d,-]+)</td>\s+"
+				strPattern3+=r"<td\s?>(?P<No3>.{1,10})</td><td\s?>(?P<Name3>.{2,20})</td><td\s{1,10}class=\"td-right\">(?P<Volume3>[0-9,]+)</td><td\s{1,10}class=\"td-right\">(?P<Diff3>[\d,-]+)</td>\s{1,16}</tr>"
 				match3 = re.match(strPattern3,item)
 				print(match3.groupdict())
 				#date,product
 				contract = "all"
 				exchange = "dce"
 				#{'No1': '20', 'Name1': '宏源期货', 'Volume1': '1,564', 'Diff1': '237', 'No2': '20', 'Name2': '安粮期货', 'Volume2': '1,449', 'Diff2': '72', 'No3': '20', 'Name3': '国富期货', 'Volume3': '1,126', 'Diff3': '92'}
-				
-				if "总计" in match3.groupdict()["No1"]:
+
+				if "总计".encode('gbk') == match3.groupdict()["No1"].encode('gbk'):
 					institution = "合计"
 					institution2 = institution
 					institution3 = institution
@@ -158,24 +160,31 @@ def parseDCE(date, datafile):
 					institution2 = match3.groupdict()["Name2"]
 					institution3 = match3.groupdict()["Name3"]
 					rank = int(match3.groupdict()["No1"])
-					volume = bigint(match3.groupdict()["Volume1"])
-					increment = bigint(match3.groupdict()["Diff1"])
-					datatype = "trade"
-					row = [date,product,contract,exchange,institution,rank,volume,increment,datatype]
-					if row:
-						csv_write.writerow(row)
-					volume2 = bigint(match3.groupdict()["Volume2"])
-					increment2 = bigint(match3.groupdict()["Diff2"])
-					datatype2 = "buy"
-					row = [date,product,contract,exchange,institution2,rank,volume2,increment2,datatype2]
-					if row:
-						csv_write.writerow(row)
-					volume3 = bigint(match3.groupdict()["Volume3"])
-					increment3 = bigint(match3.groupdict()["Diff3"])
-					datatype3 = "sell"
-					row = [date,product,contract,exchange,institution3,rank,volume3,increment3,datatype3]
-					if row:
-						csv_write.writerow(row)
+				volume = bigint(match3.groupdict()["Volume1"])
+				increment = bigint(match3.groupdict()["Diff1"])
+				datatype = "trade"
+				row = [date,product,contract,exchange,institution,rank,volume,increment,datatype]
+				if row:
+					#csv_write.writerow(row)
+					allrows.append(row)
+				volume2 = bigint(match3.groupdict()["Volume2"])
+				increment2 = bigint(match3.groupdict()["Diff2"])
+				datatype2 = "buy"
+				row = [date,product,contract,exchange,institution2,rank,volume2,increment2,datatype2]
+				if row:
+					#csv_write.writerow(row)
+					allrows.append(row)
+				volume3 = bigint(match3.groupdict()["Volume3"])
+				increment3 = bigint(match3.groupdict()["Diff3"])
+				datatype3 = "sell"
+				row = [date,product,contract,exchange,institution3,rank,volume3,increment3,datatype3]
+				if row:
+					#csv_write.writerow(row)
+					allrows.append(row)
+	allrows.sort(key=lambda x:x[1]+x[2])
+	#print(allrows[:5])
+	for row in allrows:#if int(row[6])>0:
+		csv_write.writerow(row)
 
 def parseCZCE(date, datafile):
 	file = open(datafile,'r',encoding='utf-8')
@@ -391,7 +400,6 @@ if __name__ == "__main__":
 	today = date.today().strftime('%Y%m%d')
 	today = '20200415'
 	print(today)
-	'''
 	url_SHFE = 'http://www.shfe.com.cn/data/dailydata/kx/pm%s.dat'%today
 	request.urlretrieve(url_SHFE, url_SHFE.split('/')[-1])
 	print(url_SHFE.split('/')[-1])
@@ -409,7 +417,7 @@ if __name__ == "__main__":
 	file_czce = url_czce.split('/')[-1].split(".")[0] + "_" + today + ".txt"
 	#request.urlretrieve(url_czce, file_czce)
 	downloadFile(url_czce, file_czce)
-	parseCZCE(today, file_czce)'''
+	parseCZCE(today, file_czce)
 	
 	url_dce = "http://www.dce.com.cn/publicweb/quotesdata/exportMemberDealPosiQuotesBatchData.html"
 	getDceZipData(url_dce, today, today+"_DCE_DPL.zip")
--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-import re
+import re,csv,sys
+from datetime import *
+from getFuturePositionRank import bigint
+header = ["date","product","contract","exchange","institution","rank","volume","increment","type"]
 
-f=open('content_a','r')
+#print(sys.getdefaultencoding())
+
+#f_log=open('test.log','w+',encoding='GBK')
+
+f=open('content_a','r',encoding='GBK')
 content=f.read()
 f.close()
 #print(content)
-
+csv_write = csv.writer(open("test_dce.csv",'w+',encoding='GBK',newline=''))
+csv_write.writerow(header)
+product = "a"
+date = '20200415'
+allrows = []
+strPattern22 = r"<tr >\s{1,8}<td >[0-9]{1,8}</td><td >.{2,20}</td><td  class=\"td-right\">[0-9,]+</td>\s+"
+strPattern22 += r"<td  class=\"td-right\">[0-9,]+</td>\s+<td >[0-9]+</td><td >.{2,20}</td>\s+"
+strPattern22 += r"<td  class=\"td-right\">[0-9,]+</td><td  class=\"td-right\">[0-9,]+</td>\s+<td >[0-9]+</td>\s+"
+strPattern22 += r"<td >.{2,20}</td><td  class=\"td-right\">[0-9,]+</td><td  class=\"td-right\">[0-9,]+</td>\s+</tr>"
+'''
 strPattern1 = r".*<th> 名次</th>(?P<table>.*)</table>.*"
 match1 = re.match(strPattern1,content)
 if match1:
@@ -41,3 +57,67 @@ if match1:
 		match3 = re.match(strPattern3,item)
 		print(match3)
 		print(match3.groupdict())
+'''
+strPattern1 = r".*<th> 名次</th>(?P<table>.*)</table>.*"
+match1 = re.match(strPattern1,content)
+if match1:
+	table = match1.groupdict()["table"]
+	#f_log.write(f"{table}\r\n")
+	#strPattern2 = "<tr >      <td >[0-9]+</td><td >.{2,20}</td><td  class=\"td-right\">[0-9,]+</td><td  class=\"td-right\">[0-9,]+</td>             \
+#<td >[0-9]+</td><td >.{2,20}</td><td  class=\"td-right\">[0-9,]+</td><td  class=\"td-right\">[0-9,]+</td>              \
+#<td >[0-9]+</td><td >.{2,20}</td><td  class=\"td-right\">[0-9,]+</td><td  class=\"td-right\">[0-9,]+</td>              </tr>"
+	strPattern2=r"<tr >\s{1,10}<td\s?>.{1,10}</td><td\s?>.{2,20}</td><td\s{1,10}class=\"td-right\">[0-9,]+</td><td\s{1,10}class=\"td-right\">[0-9,]+</td>\s+"
+	strPattern2+=r"<td\s?>.{1,10}</td><td\s?>.{2,20}</td><td\s{1,10}class=\"td-right\">[0-9,]+</td><td\s{1,10}class=\"td-right\">[\d,-]+</td>\s+"
+	strPattern2+=r"<td\s?>.{1,10}</td><td\s?>.{2,20}</td><td\s{1,10}class=\"td-right\">[0-9,]+</td><td\s{1,10}class=\"td-right\">[\d,-]+</td>\s{1,16}</tr>"
+	match2 = re.findall(strPattern2,table)
+	print(len(match2))
+	for item in match2:
+		strPattern3=r"<tr >\s{1,10}<td\s?>(?P<No1>.{1,10})</td><td\s?>(?P<Name1>.{2,20})</td><td\s{1,10}class=\"td-right\">(?P<Volume1>[0-9,]+)</td><td\s{1,10}class=\"td-right\">(?P<Diff1>[0-9,]+)</td>\s+"
+		strPattern3+=r"<td\s?>(?P<No2>.{1,10})</td><td\s?>(?P<Name2>.{2,20})</td><td\s{1,10}class=\"td-right\">(?P<Volume2>[0-9,]+)</td><td\s{1,10}class=\"td-right\">(?P<Diff2>[\d,-]+)</td>\s+"
+		strPattern3+=r"<td\s?>(?P<No3>.{1,10})</td><td\s?>(?P<Name3>.{2,20})</td><td\s{1,10}class=\"td-right\">(?P<Volume3>[0-9,]+)</td><td\s{1,10}class=\"td-right\">(?P<Diff3>[\d,-]+)</td>\s{1,16}</tr>"
+		#print(item)
+		match3 = re.match(strPattern3,item)
+		#print(match3.groupdict())
+		#date,product
+		contract = "all"
+		exchange = "dce"
+		#{'No1': '20', 'Name1': '宏源期货', 'Volume1': '1,564', 'Diff1': '237', 'No2': '20', 'Name2': '安粮期货', 'Volume2': '1,449', 'Diff2': '72', 'No3': '20', 'Name3': '国富期货', 'Volume3': '1,126', 'Diff3': '92'}
+
+		print(match3.groupdict()["No1"])
+		if "总计".encode('gbk') == match3.groupdict()["No1"].encode('gbk'):
+			institution = "合计"
+			institution2 = institution
+			institution3 = institution
+			print(match3.groupdict())
+			rank = 999
+		else:
+			institution = match3.groupdict()["Name1"]
+			institution2 = match3.groupdict()["Name2"]
+			institution3 = match3.groupdict()["Name3"]
+			rank = int(match3.groupdict()["No1"])
+		volume = bigint(match3.groupdict()["Volume1"])
+		increment = bigint(match3.groupdict()["Diff1"])
+		datatype = "trade"
+		row = [date,product,contract,exchange,institution,rank,volume,increment,datatype]
+		if row:
+			#csv_write.writerow(row)
+			allrows.append(row)
+		volume2 = bigint(match3.groupdict()["Volume2"])
+		increment2 = bigint(match3.groupdict()["Diff2"])
+		datatype2 = "buy"
+		row = [date,product,contract,exchange,institution2,rank,volume2,increment2,datatype2]
+		if row:
+			#csv_write.writerow(row)
+			allrows.append(row)
+		volume3 = bigint(match3.groupdict()["Volume3"])
+		increment3 = bigint(match3.groupdict()["Diff3"])
+		datatype3 = "sell"
+		row = [date,product,contract,exchange,institution3,rank,volume3,increment3,datatype3]
+		if row:
+			#csv_write.writerow(row)
+			allrows.append(row)
+allrows.sort(key=lambda x:x[1]+x[2])
+#print(allrows[:5])
+for row in allrows:#if int(row[6])>0:
+	csv_write.writerow(row)
+#print(allrows)
